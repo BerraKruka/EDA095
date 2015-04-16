6f6fe97eec2ac9aeae9cc4e2b1f46fd5faa05fcd
mat10tng <mat10tng@gmail.com>
2015-04-17 00:03:12 +0200

    Refractor some

--- NetworkProject/src/gameState/GameStart.java
+++ NetworkProject/src/gameState/GameStart.java
@@ -17,28 +17,30 @@ import org.newdawn.slick.tiled.TiledMap;
 public class GameStart extends BasicGameState {
 	private StateBasedGame game; // stored for later use
 
-    /** The collision map indicating which tiles block movement - generated based on tile properties */
+	/**
+	 * The collision map indicating which tiles block movement - generated based
+	 * on tile properties
+	 */
 	private TiledMap grassMap;
-    private boolean[][] blocked;
-    
-    private static final int SIZE = 34;
+	private boolean[][] blocked;
+
+	private static final int SIZE = 34;
 
 	// animation for movement
 	private Animation sprite, up, down, left, right;
-    private float x = 34f, y = 34f;
-
-
-
+	private float x = 34f, y = 34f;
 
 	public final static int ID = 1;
 
-
 	private void initAnimation() throws SlickException {
-		Image [] movementUp = {new Image("data/back.png"), new Image("data/back1.png")};
-        Image [] movementDown = {new Image("data/front.png"), new Image("data/front1.png")};
-        Image [] movementLeft = {new Image("data/left.png"), new Image("data/left1.png")};
-        Image [] movementRight = {new Image("data/right.png"), new Image("data/right1.png")};
-        int [] duration = {300, 300};   
+		Image[] movementUp = { new Image("data/back.png"),new Image("data/back1.png") };
+		Image[] movementDown = { new Image("data/front.png"),new Image("data/front1.png") };
+		Image[] movementLeft = { new Image("data/left.png"),
+				new Image("data/left1.png") };
+		Image[] movementRight = { new Image("data/right.png"),
+				new Image("data/right1.png") };
+
+		int[] duration = { 300, 300 };
 
 		/*
 		 * false variable means do not auto update the animation. By setting it
@@ -49,7 +51,7 @@ public class GameStart extends BasicGameState {
 		left = new Animation(movementLeft, duration, false);
 		right = new Animation(movementRight, duration, false);
 
-		// Original orientation of the sprite. It will look right.
+		// this will depend on the player later on.
 		sprite = right;
 	}
 
@@ -80,81 +82,59 @@ public class GameStart extends BasicGameState {
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-	      grassMap.render(0, 0);
-	      sprite.draw((int)x, (int)y);
+		grassMap.render(0, 0);
+		sprite.draw((int) x, (int) y);
 	}
 
-	
+	private void action(Animation direction, int deltaX, int deltaY) {
+		// set the direction of player after movement
+		sprite = direction;
+		float xNew = x + deltaX * 0.1f;
+		float yNew = y + deltaY * 0.1f;
+
+		if (!isBlocked(xNew, yNew)) {
+			sprite.update(deltaX + deltaY);
+			y = yNew;
+			x = xNew;
+		}
+	}
+
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
-		 Input input = container.getInput();
-	        if (input.isKeyDown(Input.KEY_UP))
-	        {
-	            sprite = up;
-	            if (!isBlocked(x, y - delta * 0.1f))
-	            {
-	                sprite.update(delta);
-	                // The lower the delta the slowest the sprite will animate.
-	                y -= delta * 0.1f;
-	            }
-	        }
-	        else if (input.isKeyDown(Input.KEY_DOWN))
-	        {
-	            sprite = down;
-	            if (!isBlocked(x, y + SIZE + delta * 0.1f))
-	            {
-	                sprite.update(delta);
-	                y += delta * 0.1f;
-	            }
-	        }
-	        else if (input.isKeyDown(Input.KEY_LEFT))
-	        {
-	            sprite = left;
-	            if (!isBlocked(x - delta * 0.1f, y))
-	            {
-	                sprite.update(delta);
-	                x -= delta * 0.1f;
-	            }
-	        }
-	        else if (input.isKeyDown(Input.KEY_RIGHT))
-	        {
-	            sprite = right;
-	            if (!isBlocked(x + SIZE + delta * 0.1f, y))
-	            {
-	                sprite.update(delta);
-	                x += delta * 0.1f;
-	            }
-	        }
-		
+		Input input = container.getInput();
+
+		if (input.isKeyDown(Input.KEY_UP)) {
+			action(up, 0, -delta);
+		} else if (input.isKeyDown(Input.KEY_DOWN)) {
+			action(down, 0, delta);
+		} else if (input.isKeyDown(Input.KEY_LEFT)) {
+			action(left, -delta, 0);
+		} else if (input.isKeyDown(Input.KEY_RIGHT)) {
+			action(right, delta, 0);
+		}
 	}
 
 	private boolean isBlocked(float f, float y2) {
-	     int xBlock = (int)x / SIZE;
-	        int yBlock = (int)y / SIZE;
-	        return blocked[xBlock][yBlock];		
+		int xBlock = (int) x / SIZE;
+		int yBlock = (int) y / SIZE;
+		return blocked[xBlock][yBlock];
 	}
 
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
 		return ID;
 	}
-	  public void keyReleased(int key, char c) {
-	        switch(key) {
-	        case Input.KEY_EQUALS:
-	            game.enterState(Menu.ID, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
-	            break;
-	        case Input.KEY_2:
-	            // TODO: Implement later
-	            break;
-	        case Input.KEY_3:
-	            // TODO: Implement later
-	            break;
-	        default:
-	            break;
-	        }
-	    }
-	
+
+	public void keyReleased(int key, char c) {
+		switch (key) {
+		case Input.KEY_ESCAPE:
+			game.enterState(Menu.ID, new FadeOutTransition(Color.black),
+					new FadeInTransition(Color.black));
+			break;
+		default:
+			break;
+		}
+	}
 
 }
